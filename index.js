036896887e44e2941d40c83c11dee05e34b221bb
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-08-26 15:39:47 -0400

    added to view in index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+// ------ View ------
+
+const showContent (() => {
+    const appContent = {
+        courseContainer: '#course-container',
+        conent: '.content',
+    }
+})
+
+
+
+
+const render = (ele, tmp) => {
+    ele.innerHTML = tmp;
+};
+
+const makeTemplete = (arr) => {
+    let tmp = '';
+    arr.array.forEach((course) => {
+        tmp += `
+            <li>
+            <span>${course.courseId}</span>
+            </li>
+            <li>
+            <span>${course.CourseName}</span>
+            </li> 
+            <li>
+            <span>${course.credit}</span>
+            </li>
+            `;
+    });
+    return tmp;
+};
+return {
+    appContent,
+    render,
+    createTmp,
+};
+
+makeTemplete();
+
+// ------- Model --------
+const Model = ((api, view) => {
+    class Courses {
+        constructor(title) {
+            this.courseId = 1;
+            this.CourseName = name;
+            this.required = true;
+            this.credit = Number;
+        }
+    }
+
+    class State {
+        #courses = [];
+        get courses() {
+            return this.#courses;
+        }
+        set courses(availableCourses) {
+            this.#courses = [...availableCourses];
+        }
+    }
+})
+
+
+
+// ------- Controller --------
